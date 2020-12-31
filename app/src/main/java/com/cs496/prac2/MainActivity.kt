b8d34f9b9c9065f52367d796bdf43213ea9afcc1
YewonChoi <woni9911@gmail.com>
2020-12-31 16:08:33 +0900

    Basic functionality
    stop button not work/ record not saved on destroy

---
+++ app/src/main/java/com/cs496/prac2/MainActivity.kt
@@ -0,0 +1,174 @@
+package com.cs496.prac2
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.IBinder
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.size
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var isRunning = false
+    private var isNotZero = false
+    private var timerTask: Timer? = null
+    private var index :Int = 0
+
+    private var timeText: TextView? = null
+    var startBtn: Button? = null
+    var recodeBtn: Button? = null
+    var stopBtn: Button? = null
+    var resetBtn: Button? = null
+    var recodeText: LinearLayout? = null
+
+    private lateinit var mService: MyService
+    private var mBound: Boolean = false
+
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as MyService.MyBinder
+            mService = binder.getService()
+            mBound = true
+
+            isRunning = mService.getIsRunning()
+            isNotZero = mService.getIsNotZero()
+            if (isNotZero) {
+                resume()
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mBound = false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        timeText = findViewById(R.id.time_text)
+        startBtn = findViewById(R.id.start_btn)
+        recodeBtn = findViewById(R.id.recode_btn)
+        stopBtn = findViewById(R.id.stop_btn)
+        resetBtn = findViewById(R.id.reset_btn)
+        recodeText = findViewById(R.id.recode_text)
+        val container: LinearLayout = findViewById(R.id.container)
+        var num: Int = container.size
+
+        startBtn?.setOnClickListener {
+            isRunning = !isRunning
+            isNotZero = true
+            if (isRunning) start() else pause()
+        }
+
+        recodeBtn?.setOnClickListener {
+            if(isRunning) lapTime()
+        }
+
+        stopBtn?.setOnClickListener {
+            when (num) {
+                0-> {
+                    val textView = TextView(this).apply {
+                        setTextSize(20f)
+                    }
+                    textView.text = "new tv"
+                    container.addView(textView,0)
+                    num++
+                }
+                1-> {
+                    container.removeAllViews()
+                    num--
+                }
+            }
+
+        }
+
+        resetBtn?.setOnClickListener{
+            reset();
+        }
+
+        startService(Intent(this, MyService::class.java))
+        bindService(Intent(this, MyService::class.java), connection, Context.BIND_AUTO_CREATE)
+
+
+    }
+
+    private fun setTime() {
+        val setTime = mService.getTime()
+        var milli = setTime % 100
+        var sec = (setTime/100) % 60
+        var min = setTime / 6000
+        timeText?.text = "$min:$sec.$milli"
+    }
+
+    private fun lapTime() {
+        val lapTime = mService.getTime()
+        val textView = TextView(this).apply {
+            setTextSize(20f)
+        }
+        textView.text = "${lapTime / 6000}:${(lapTime/100) % 60}.${lapTime % 100}"
+
+        recodeText?.addView(textView,0)
+        index++
+    }
+
+    private fun start() {
+        mService.startAndPause()
+        timerTask = kotlin.concurrent.timer(period = 10) {
+            runOnUiThread {
+                setTime()
+            }
+        }
+    }
+
+    private fun resume() {
+        when (isRunning) {
+            true -> {
+                timerTask = kotlin.concurrent.timer(period = 10) {
+                    runOnUiThread {
+                        setTime()
+                    }
+                }
+            }
+            false -> {
+                setTime()
+            }
+        }
+
+    }
+
+    private fun pause() {
+        mService.startAndPause()
+        timerTask?.cancel()
+    }
+
+    private fun reset() {
+        mService.reset()
+        timerTask?.cancel()
+
+        isRunning = false
+        isNotZero = false
+        timeText?.text = "00:00.00"
+
+        recodeText?.removeAllViews()
+        index = 0
+    }
+
+    override fun onDestroy() {
+        if (!isNotZero) {
+            stopService(Intent(this, MyService::class.java))
+        }
+        super.onDestroy()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+}
---
+++ app/src/main/java/com/cs496/prac2/MyService.kt
@@ -0,0 +1,84 @@
+package com.cs496.prac2
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MyService: Service() {
+
+    private var time = 0
+    private var isNotZero = false
+    private var isRunning = false
+    private var timerTask: Timer? = null
+    private var id: Int = 0
+
+    //val records: ArrayList<Int> = ArrayList
+    private val myBinder = MyBinder()
+
+    inner class MyBinder: Binder() {
+        fun getService(): MyService = this@MyService
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        Log.d("service($id)","onBind()")
+        return myBinder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        Log.d("service($id)","onStartCommand()")
+        return Service.START_STICKY
+    }
+
+    override fun onCreate() {
+        id = Random().nextInt(100)
+        Log.d("service($id)","onCreate()")
+        super.onCreate()
+    }
+
+    override fun onDestroy() {
+        Log.d("service($id)","onDestroy()")
+        super.onDestroy()
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d("service($id)","onUnbind()")
+        return super.onUnbind(intent)
+    }
+
+    fun startAndPause() {
+        when (isRunning) {
+            false -> timerTask = kotlin.concurrent.timer(period = 10) {
+                    time++ }
+            true -> timerTask?.cancel()
+        }
+        isRunning = !isRunning
+        isNotZero = true
+    }
+
+    fun reset() {
+        isRunning = false
+        isNotZero = false
+        time = 0
+    }
+
+    fun addRecord() {
+
+    }
+
+    fun getIsRunning(): Boolean {
+        return isRunning
+    }
+
+    fun getIsNotZero(): Boolean {
+        return isNotZero
+    }
+
+    fun getTime(): Int {
+        return time
+    }
+}
